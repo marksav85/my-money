648030a49ef670f6fba9b8ac4ddac97d6d031323
Mark Saville <mark.saville@outlook.com>
2024-02-08 13:29:04 +0100

    Add Navbar component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
+// components
+import Navbar from "./components/Navbar";
+
 // pages
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
-
-// styles
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />

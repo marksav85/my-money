53425f51ee8038eb492c829fa81cb5e210abab20
Mark Saville <mark.saville@outlook.com>
2024-02-08 13:07:31 +0100

    Refactor component and file names in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
-// components
-import Home from "./pages/home/home";
-import Login from "./pages/login/login";
-import Signup from "./pages/signup/signup";
+// pages
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 // styles
 import "./App.css";
 

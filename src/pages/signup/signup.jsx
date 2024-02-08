00cca2fda04b6883734a82662699bd3354d65c4f
Mark Saville <mark.saville@outlook.com>
2024-02-08 13:59:56 +0100

    Add signup form with input fields and form submission handling

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
+// styles
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>signup</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("name:", name);
+    console.log("email:", email);
+    console.log("password:", password);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Signup</h2>
+      <label>
+        <span>Name:</span>
+        <input
+          type="name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }

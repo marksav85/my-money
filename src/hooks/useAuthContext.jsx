9ad893d39b386ce28c88aac3aac2a73b285f2c65
Mark Saville <mark.saville@outlook.com>
2024-02-09 10:20:57 +0100

    Add error handling for missing AuthContextProvider

--- src/hooks/useAuthContext.jsx
+++ src/hooks/useAuthContext.jsx
@@ -4,6 +4,7 @@ import { useContext } from "react";
 export const useAuthContext = () => {
   const context = useContext(AuthContext);
 
+  // error in case the context is not used within AuthContextProvider
   if (!context) {
     throw new Error(
       "useAuthContext must be used within an AuthContextProvider"

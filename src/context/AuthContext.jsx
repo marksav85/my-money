13b9d8fc9911692e553cbcc9ed55908ccf7f013a
Mark Saville <mark.saville@outlook.com>
2024-02-12 11:54:46 +0100

    Add AUTH_IS_READY action to AuthReducer

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -15,6 +15,8 @@ export const AuthReducer = (state, action) => {
         ...state,
         user: null,
       };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }

c82273d65789f9a4d8207a6db4f2b6b4910c3506
Mark Saville <mark.saville@outlook.com>
2024-02-14 10:13:05 +0100

    Refactor home page layout

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,10 @@
 import styles from "./Home.module.css";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction List</div>
+      <div className={styles.sidebar}></div>
+    </div>
+  );
 }

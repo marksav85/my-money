08a29d7403805bfda572034a36bac6d0565b0258
Mark Saville <mark.saville@outlook.com>
2024-02-14 10:32:17 +0100

    Add TransactionForm component to sidebar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,16 @@
+// components
+import TransactionForm from "./TransactionForm";
+
+// styles
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>Transaction List</div>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   );
 }

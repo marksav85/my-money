496cd78252ff3d841f1eeec5b1a93cc1cac536df
Mark Saville <mark.saville@outlook.com>
2024-02-21 14:52:59 +0100

    Add useRef to avoid re-rendering loop in useEffect dependency array

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -1,14 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { projectFirestore } from "../firebase/config";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 
-export const useCollection = (collectionData) => {
+export const useCollection = (collectionData, collectionQuery) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
+
+  // use useRef to store the query to avoid re-rendering loop in useEffect dependency array
+  const q = useRef(collectionQuery).current;
+
+  useEffect(() => {
+    // Update the ref when collectionQuery changes
+    q.current = collectionQuery;
+  }, [collectionQuery, q]);
 
   useEffect(() => {
     // collection ref
     let ref = collection(projectFirestore, collectionData);
+
+    // queries user id before retrieving collection data
+    if (q.current) {
+      ref = query(ref, where(...q.current));
+    }
 
     const unsubscribe = onSnapshot(
       ref,
@@ -31,7 +44,7 @@ export const useCollection = (collectionData) => {
 
     // unsubscribe from snapshot when no longer in use
     return () => unsubscribe();
-  }, [collectionData]);
+  }, [collectionData, q]);
 
   return { documents, error };
 };
